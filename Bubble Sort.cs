ad233d67513d59639ff5d902b3f05561da13454e
Rushiu123 <rushikesh.ubalkar@gmail.com>
2023-01-23 18:05:05 +0530

    AlgorithmPrograme

---
+++ Bubble Sort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Programs
+{
+    public class BubbleSort
+    {
+        public void BubbleSortFn()
+        {
+            int[] arr = { 15, 31, 24, 12, 45, 30 };
+            int temp;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // swap the elements
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            // Print the sorted array
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\nWelcome to Algorithm Problems\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.WriteLine("1.Anagram.\n2.Binary Search.\n3. Bubble Sort.\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1: //Anagram Detection 
+                    Anagram anagram = new Anagram();
+                    anagram.CheckAnagram();
+                    break;
+                case 2: //Binary Search 
+                    BinarySearch binarySearch = new BinarySearch();
+                    binarySearch.BinarySearchFn();
+                    break;
+                case 3: //Bubble Sort 
+                    BubbleSort bubbleSort = new BubbleSort();
+                    bubbleSort.BubbleSortFn();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option");
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
